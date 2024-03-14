d9cadbbcc8b56d92fb74a4ec52a25158d397575a
Prakash Kaliyannan <prakas1103@gmail.com>
2024-03-14 12:21:51 +0530

    First commit

---
+++ src/test/java/com/example/gradlegroovydemo/GradlegroovydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlegroovydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlegroovydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
